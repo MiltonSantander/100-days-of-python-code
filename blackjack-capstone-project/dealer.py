8260002827a6cd6b46e64eb87f7ca9c30acaf91f
MiltonGarcia <milgarsan@gmail.com>
2022-05-31 21:29:53 -0400

    Score and dealer entities implemented as classes

--- blackjack-capstone-project/dealer.py
+++ blackjack-capstone-project/dealer.py
@@ -1,7 +1,22 @@
 import random
 
-deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
+class Dealer:
+    def __init__(self):
+        self.cards = {
+            "full_deck": [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10],
+            "user_cards": [],
+            "cpu_cards": []
+        }
 
-def deal_card():
-    return random.choice(deck)
+    def deal_card(self):
+        return random.choice(self.cards["full_deck"])
+
+    def serve_new_hand(self):
+        for _ in range(2):
+            self.cards["user_cards"].append(self.deal_card())
+            self.cards["cpu_cards"].append(self.deal_card())
+
+    def change_ace_value(self, cards):
+        cards.remove(11)
+        cards.append(1)
--- blackjack-capstone-project/score.py
+++ blackjack-capstone-project/score.py
@@ -1,8 +1,16 @@
-def calculate_score(cards):
-    score = 0
-    for index, card in enumerate(cards):
-        score += card
-        if score > 21 and card == 11:
-            score -= 10
-            cards[index] = 1
-    return score, cards
+class Score:
+    def __init__(self):
+        self.scores = {
+            "user_score": 0,
+            "cpu_score": 0
+        }
+
+    def calculate_score(self, user_cards, cpu_cards):
+        self.scores["user_score"] = 0
+        self.scores["cpu_score"] = 0
+
+        for card in user_cards:
+            self.scores["user_score"] += card
+
+        for card in cpu_cards:
+            self.scores["cpu_score"] += card

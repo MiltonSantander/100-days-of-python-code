1b8af0a960e16882c8bbb6a8e11185d08d69e191
MiltonGarcia <milgarsan@gmail.com>
2022-07-25 23:38:43 -0400

    Now the score class uses an instance of the dealer class to calculate the score and to use the change_ace_value function

--- blackjack-capstone-project/score.py
+++ blackjack-capstone-project/score.py
@@ -5,12 +5,18 @@ class Score:
             "cpu_score": 0
         }
 
-    def calculate_score(self, user_cards, cpu_cards):
+    def calculate_score(self, dealer_instance):
         self.scores["user_score"] = 0
         self.scores["cpu_score"] = 0
 
-        for card in user_cards:
+        for card in dealer_instance.cards["user_cards"]:
             self.scores["user_score"] += card
+            if self.scores["user_score"] > 21 and 11 in dealer_instance.cards["user_cards"]:
+                dealer_instance.change_ace_value(True)
+                self.calculate_score(dealer_instance)
 
-        for card in cpu_cards:
+        for card in dealer_instance.cards["cpu_cards"]:
             self.scores["cpu_score"] += card
+            if self.scores["cpu_score"] > 21 and 11 in dealer_instance.cards["cpu_cards"]:
+                dealer_instance.change_ace_value(False)
+                self.calculate_score(dealer_instance)
